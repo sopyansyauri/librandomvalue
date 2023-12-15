9f43a20d36c871090c3de53e9e45a4e0cefac6ea
sopyansyauri <rifaii.sopyansyauri04@gmail.com>
2023-12-15 08:39:21 +0700

    Edit file: sr/random_value.c & tests/test.c

--- src/random_value.c
+++ src/random_value.c
@@ -1,15 +1,12 @@
 #include <stdlib.h>
-#include <time.h>
 #include "random_value.h"
 
 int random_int()
 {
-	srand(time(0));
 	return rand();
 }
 
 double random_float()
 {
-	srand(time(0));
 	return (double)rand() / (double)RAND_MAX;
 }
--- tests/test.c
+++ tests/test.c
@@ -1,10 +1,54 @@
 #include <stdio.h>
 #include <random_value.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-    printf("%d \n", random_int());
-    printf("%f \n", random_float());
+    int number_int[2][2];
+    double number_double[2][2];
+
+    int i, j;
+
+    // In order to get random value
+    srand(time(0));
+    for (i = 0; i < sizeof(number_int) / sizeof(number_int[0]); i++)
+    {
+        for (j = 0; j < sizeof(number_int[0]) / sizeof(number_int[0][0]); j++)
+        {
+            number_int[i][j] = random_int();
+        }
+    }
+
+    for (i = 0; i < sizeof(number_int) / sizeof(number_int[0]); i++)
+    {
+        for (j = 0; j < sizeof(number_int[0]) / sizeof(number_int[0][0]); j++)
+        {
+            printf("%d ", number_int[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    // In order to get random value
+    srand(time(0));
+    for (i = 0; i < sizeof(number_double) / sizeof(number_double[0]); i++)
+    {
+        for (j = 0; j < sizeof(number_double[0]) / sizeof(number_double[0][0]); j++)
+        {
+            number_double[i][j] = random_float();
+        }
+    }
+
+    for (i = 0; i < sizeof(number_double) / sizeof(number_double[0]); i++)
+    {
+        for (j = 0; j < sizeof(number_double[0]) / sizeof(number_double[0][0]); j++)
+        {
+            printf("%f ", number_double[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
